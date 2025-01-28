3d0b7995ee685cdd584ae33080f22c1c4850eb8a
ris8z <giuseppe.esposito2@mail.dcu.ie>
2025-01-28 19:31:32 +0000

    usered and elections table updated while created election_credits and candidates tables

--- code/blockchain-voting-system/backend/models.py
+++ code/blockchain-voting-system/backend/models.py
@@ -1,14 +1,48 @@
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, Text, Boolean, ForeignKey
 from extensions import db
+import enum
+
+class ElectionStatus(enum.Enum):
+    UPCOMING = 'upcoming'
+    ONGOING = 'ongoing'
+    COMPLETED ='completed'
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(10), nullable=False)
-    credits = db.Column(db.Integer, default=100)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullabe=False)
+    is_admin: Mapped[Boolean] = mapped_column(Boolean, default=False) 
 
 class Election(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    status = db.Column(db.String(20), nullable=False)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), nullable=False)
+    start_credits: Mapped[int] = mapped_column(Integer, nullable=False)
+    start_date: Mapped[int] = mapped_column(Integer, nullable=False) 
+    end_date: Mapped[int] = mapped_column(Integer, nullable=False) 
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+
+    @property
+    def status(self) -> ElectionStatus:
+        """ Evaluate current status dinamimcally """
+        now = int(datetime.utcnow().timestamp())
+
+        if now < self.start_date:
+            return ElectionStatus.UPCOMING
+        elif self.start_date <= now <= self.end_date:
+            return ElectionStatus.ONGOING
+        else:
+            return ElectionStatus.COMPLETED
+        
+class ElectionCredits(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'), nullable=False)
+    election_id: Mapped[int] = mapped_column(Integer, ForeignKey('election.id'), nullable=False)
+    credits_left: Mapped[int] = mapped_column(Integer, nullable=False)
+
+class Candidates(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    election_id: Mapped[int] = mapped_column(Integer, ForeignKey('election.id'), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
